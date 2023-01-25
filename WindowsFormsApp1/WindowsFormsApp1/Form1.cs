478d9f580f209ea87e72a374e3f6f63ad51fbe15
SCP-939-83 <512707@vistacollege.nl>
2023-01-25 14:08:34 +0100

    alot got updated

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -12,12 +12,12 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        public Form1();
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs)
         {
 
         }
